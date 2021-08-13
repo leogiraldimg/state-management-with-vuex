fd7a4485ebe3c91e77f783ce5221ed38dbc9788a
Leonardo Giraldi <leogiraldimg@gmail.com>
2021-08-13 07:39:05 -0300

    ✨ creates a vuex store

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import * as Vue from 'vue';
-import * as VueRouter from 'vue-router';
+import * as Vue from "vue";
+import * as VueRouter from "vue-router";
 
-import App from './App.vue';
-import routes from './routes';
+import App from "./App.vue";
+import routes from "./routes";
+import store from "./store";
 
 const router = VueRouter.createRouter({
   history: VueRouter.createWebHistory(),
-  routes,
+  routes
 });
 
 Vue.createApp(App)
   .use(router)
-  .mount('#app');
+  .use(store)
+  .mount("#app");
