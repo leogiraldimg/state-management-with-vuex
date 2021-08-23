c23f71ad6a670951a8764f2a4e63398465ada2ae
leogiraldimg@gmail.com <leogiraldimg@gmail.com>
2021-08-23 08:26:36 -0300

    :sparkles: creates vuex modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,48 +1,19 @@
 import { createStore } from 'vuex';
-import axios from 'axios';
 
-import filterProducts from '../products/filter-products';
+import productsModule from './modules/products';
+import usersModule from './modules/users';
 
 const store = createStore({
+  modules: {
+    products: productsModule,
+    users: usersModule,
+  },
   state() {
-    return {
-      user: null,
-      products: null,
-    };
+    return {};
   },
-  getters: {
-    getFilteredProducts(state) {
-      return (filter) => filterProducts(filter, state.products);
-    },
-  },
-  mutations: {
-    setUser(state, user) {
-      state.user = user;
-    },
-    setProducts(state, products) {
-      state.products = products;
-    },
-  },
-  actions: {
-    fetchProducts({ commit }) {
-      axios.get('/api/products').then((result) => {
-        commit('setProducts', result.data);
-      });
-    },
-    registerUser({ commit }, user) {
-      return axios.post('/api/register', user).then((result) => {
-        commit('setUser', result.data);
-      });
-    },
-    signIn({ commit }, userLogin) {
-      return axios.post('/api/sign-in', userLogin).then((result) => {
-        commit('setUser', result.data);
-      });
-    },
-    signOut({ commit }, userLogin) {
-      return axios.post('/api/sign-out', userLogin).then(() => commit('setUser', null));
-    },
-  },
+  getters: {},
+  mutations: {},
+  actions: {},
 });
 
 export default store;
